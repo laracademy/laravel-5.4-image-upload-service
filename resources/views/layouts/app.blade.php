62ecaba8ae202b61ed70c468837940f71d5b692e
Michael McMullen <michael.mcmullen@tutelagesystems.com>
2017-08-06 11:01:40 -0400

    HTML Code for lesson 5
    Bootstrap, jQuery, Master Template, Navigation and Welcome page

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>{{ config('app.name') }}</title>
+
+        <link rel='stylesheet' href='//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/css/bootstrap.min.css'>
+        <link rel='stylesheet' href='//bootswatch.com/4-alpha/lux/bootstrap.css'>
+        <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    </head>
+    <body>
+
+        @include('layouts.partials.navigation')
+
+        @yield('content')
+
+        <script src='//cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js'></script>
+        <script src='//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/js/bootstrap.min.js'></script>
+    </body>
+</html>
---
+++ resources/views/layouts/partials/navigation.blade.php
@@ -0,0 +1,17 @@
+<nav class="navbar navbar-toggleable-md navbar-inverse bg-primary">
+    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbar">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand" href="#">{{ config('app.name') }}</a>
+
+    <div class="collapse navbar-collapse" id="navbar">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="{{ route('home') }}">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Upload Image</a>
+            </li>
+        </ul>
+    </div>
+</nav>
